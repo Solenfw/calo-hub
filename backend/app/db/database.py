bebd13fcc9c577e6c6f80a7f75c17943641bbfbc
solenfw <nhuyhoang1012005@gmail.com>
2026-06-11 04:40:29 +0700

    chore: bruh forgot -.-

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from ..core.config import settings
 
-engine = create_engine(settings.database_url, future=True)
+engine = create_engine(settings.database_url, future=True, connect_args={"sslmode": "require"} if "localhost" not in settings.database_url else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
 Base = declarative_base()
